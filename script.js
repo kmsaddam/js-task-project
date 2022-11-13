c22074e585c381e683e2eeb63e39b9c6f1363371
K M Saddam Hossain <dev.saddam@gmail.com>
2022-11-13 13:42:20 +0600

    Remove Task

--- script.js
+++ script.js
@@ -7,21 +7,30 @@ let taskInput = document.querySelector("#new_task");
 
 //Define Event Listners
 form.addEventListener('submit', addTask);
+taskList.addEventListener('click', removeTask);
 
 
 //Define Functions
 function addTask(e) {
-    
     if(taskInput.value === '') {
         alert("Add a Task...")
     } else {
         let li = document.createElement("li");
         li.appendChild(document.createTextNode(taskInput.value + " "));
+        let link = document.createElement("a");
+        link.setAttribute('href', '#');
+        link.appendChild(document.createTextNode('x'));
+        li.appendChild(link);
         taskList.appendChild(li);
-
+        taskInput.value = '';
     }
+    e.preventDefault();
+}
 
-
-
+function removeTask(e) {
+    if(e.target.hasAttribute('href')) {
+        let parentElement = e.target.parentElement;
+        parentElement.remove();
+    } 
     e.preventDefault();
 }
